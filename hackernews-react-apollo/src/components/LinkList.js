364e6232fa8b29795c21d116b142d39afb905e1d
Eder <eder.ramirez87@gmail.com>
2017-08-21 19:44:38 -0500

    Realtime updates with graphQL subscriptions

--- hackernews-react-apollo/src/components/LinkList.js
+++ hackernews-react-apollo/src/components/LinkList.js
@@ -4,40 +4,131 @@ import Link from './Link'
 
 class LinkList extends Component {
 
-    _updateCacheAfterVote = (store, createVote, linkId) => {
-        // 1
-        const data = store.readQuery({ query: ALL_LINKS_QUERY })
-        
-        // 2
-        const votedLink = data.allLinks.find(link => link.id === linkId)
-        votedLink.votes = createVote.link.votes
-        
-        // 3
-        store.writeQuery({ query: ALL_LINKS_QUERY, data })
+  _subscribeToNewVotes = () => {
+    this.props.allLinksQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          Vote(filter: {
+            mutation_in: [CREATED]
+          }) {
+            node {
+              id
+              link {
+                id
+                url
+                description
+                createdAt
+                postedBy {
+                  id
+                  name
+                }
+                votes {
+                  id
+                  user {
+                    id
+                  }
+                }
+              }
+              user {
+                id
+              }
+            }
+          }
+        }
+      `,
+      updateQuery: (previous, { subscriptionData }) => {
+        const votedLinkIndex = previous.allLinks.findIndex(link => link.id === subscriptionData.data.Vote.node.link.id)
+        const link = subscriptionData.data.Vote.node.link
+        const newAllLinks = previous.allLinks.slice()
+        newAllLinks[votedLinkIndex] = link
+        const result = {
+          ...previous,
+          allLinks: newAllLinks
+        }
+        return result
       }
+    })
+  }
 
-    render() {
-        // LOADING
-        if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
-            return <div>Loading</div>
+  _subscribeToNewLinks = () => {
+    this.props.allLinksQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          Link(filter: {
+            mutation_in: [CREATED]
+          }) {
+            node {
+              id
+              url
+              description
+              createdAt
+              postedBy {
+                id
+                name
+              }
+              votes {
+                id
+                user {
+                  id
+                }
+              }
+            }
+          }
         }
-
-        // ERROR
-        if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
-            return <div>Error</div>
+      `,
+      updateQuery: (previous, { subscriptionData }) => {
+        const newAllLinks = [
+          subscriptionData.data.Link.node,
+          ...previous.allLinks
+        ]
+        const result = {
+          ...previous,
+          allLinks: newAllLinks
         }
+        return result
+      }
+    })
+  }
 
-        // SUCCESS
-        const linksToRender = this.props.allLinksQuery.allLinks
-        console.log(linksToRender)
-        return (
-            <div>
-                {linksToRender.map((link, index) => (
-                    <Link key={link.id} updateStoreAfterVote={this._updateCacheAfterVote}  index={index} link={link}/>
-                  ))}
-            </div>
-        )
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    // 1
+    const data = store.readQuery({ query: ALL_LINKS_QUERY })
+
+    // 2
+    const votedLink = data.allLinks.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+
+    // 3
+    store.writeQuery({ query: ALL_LINKS_QUERY, data })
+  }
+
+  componentDidMount() {
+    this._subscribeToNewLinks()
+    this._subscribeToNewVotes()
+  }
+
+  render() {
+    // LOADING
+    if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
+      return <div>Loading</div>
     }
+
+    // ERROR
+    if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
+      return <div>Error</div>
+    }
+
+    // SUCCESS
+    const linksToRender = this.props.allLinksQuery.allLinks
+    console.log(linksToRender)
+    return (
+      <div>
+        {linksToRender.map((link, index) => (
+          <Link key={link.id} updateStoreAfterVote={this._updateCacheAfterVote} index={index} link={link} />
+        ))}
+      </div>
+    )
+  }
 
 }
 
--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -5,6 +5,7 @@ import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import './styles/index.css'
 import { GC_AUTH_TOKEN } from './constants'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
 
 // 1
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
@@ -13,6 +14,19 @@ import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apol
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj6jdpccp02st0165m1r0r1l5'
 })
+
+
+const wsClient = new SubscriptionClient('wss://subscriptions.us-west-2.graph.cool/v1/cj6jdpccp02st0165m1r0r1l5', {
+  reconnect: true,
+  connectionParams: {
+     authToken: localStorage.getItem(GC_AUTH_TOKEN),
+  }
+})
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+)
 
 networkInterface.use([{
   applyMiddleware(req, next) {
@@ -25,9 +39,8 @@ networkInterface.use([{
   }
 }])
 
-// 3
 const client = new ApolloClient({
-  networkInterface
+  networkInterface: networkInterfaceWithSubscriptions
 })
 
 // 4
